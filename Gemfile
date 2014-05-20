75bfdb954804ba487191b3d1e1f6aa095cbcf279
Shane Kelly <shanek6@gmail.com>
2014-05-20 12:56:25 +0100

    styled forms, installed paperclip added custom css

--- Gemfile
+++ Gemfile
@@ -11,5 +11,6 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'spring', group: :development
 gem 'bootstrap-sass', '~> 3.1.1'
+gem 'paperclip', github: 'thoughtbot/paperclip'
 
 gem 'rails_12factor', group: :production
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519142201) do
+ActiveRecord::Schema.define(version: 20140520105128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,10 @@ ActiveRecord::Schema.define(version: 20140519142201) do
     t.decimal  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
