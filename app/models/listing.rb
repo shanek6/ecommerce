8e5a756ac65dbce5055237e0c1de39ef065b5808
Shane Kelly <shanek6@gmail.com>
2014-05-22 16:21:29 +0100

    fixed dropbox in model :path

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,6 @@
 class Listing < ActiveRecord::Base
 
-	if
+	if Rails.env.development?
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "missing.jpg"
 
 	else
